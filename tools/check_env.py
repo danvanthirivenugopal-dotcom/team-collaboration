4f27f4bf73221440c86e9bb5644ea59902c60118
danvanthirivenugopal-dotcom <danvanthirivenugopal@gmail.com>
2026-06-27 20:40:20 +0530

    Remove unwanted temporary files and test scripts

--- tools/check_env.py
+++
@@ -1,4 +0,0 @@
-import sys, pkgutil
-print('executable:', sys.executable)
-print('uvicorn:', pkgutil.find_loader('uvicorn') is not None)
-print('streamlit:', pkgutil.find_loader('streamlit') is not None)
